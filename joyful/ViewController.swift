86c6869cb322a7dc399845afc5e9ea6bd8b182e6
golden <golden>
2019-05-15 08:37:00 +0700

    print kantor

---
+++ joyful/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  joyful
+//
+//  Created by Golden Kevin on 15/05/19.
+//  Copyright © 2019 gkevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let kantor = 20
+        print (kantor)
+        // Do any additional setup after loading the view.
+    }
+}
+
+
